03c594dc2a8426177505164035af2f16851309dd
Marcos Ferreira Shirafuchi <marcosfshirafuchi@gmail.com>
2025-12-22 19:51:01 -0300

    12. Seeding da base de dados

---
+++ 01 - CRUD e estruturação do projeto/12. Seeding da base de dados/backend/src/main/java/com/devsuprior/dscatalog/services/CategoryService.java
@@ -0,0 +1,23 @@
+package com.devsuprior.dscatalog.services;
+
+import com.devsuprior.dscatalog.entities.Category;
+import com.devsuprior.dscatalog.repositories.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+public class CategoryService {
+
+    //Coloca a anotação Autowired para fazer a injeção de dependencia da classe CategoryRepository
+    @Autowired
+    private CategoryRepository repository;
+
+    @Transactional(readOnly = true)
+    public List<Category> findAll() {
+        return repository.findAll();
+    }
+
+}
