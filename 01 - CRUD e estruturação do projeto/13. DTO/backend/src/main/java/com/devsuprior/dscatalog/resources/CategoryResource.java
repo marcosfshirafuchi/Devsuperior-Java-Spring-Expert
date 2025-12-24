12cb1ce60d4a4369d2c689b9ec53a56cdc034cac
Marcos Ferreira Shirafuchi <marcosfshirafuchi@gmail.com>
2025-12-24 16:48:06 -0300

    13. DTO

---
+++ 01 - CRUD e estruturação do projeto/13. DTO/backend/src/main/java/com/devsuprior/dscatalog/resources/CategoryResource.java
@@ -0,0 +1,30 @@
+package com.devsuprior.dscatalog.resources;
+
+import com.devsuprior.dscatalog.dto.CategoryDTO;
+import com.devsuprior.dscatalog.entities.Category;
+import com.devsuprior.dscatalog.services.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+//Controla as requisições da api
+@RestController
+@RequestMapping(value = "/categories")
+public class CategoryResource {
+
+    //Coloca a anotação Autowired para fazer a injeção de dependencia da classe CategoryService
+    @Autowired
+    private CategoryService service;
+
+    @GetMapping
+    public ResponseEntity<List<CategoryDTO>> findAll(){
+        // O list está buscando os registros da classe CategoryService pelo método findAll
+        List<CategoryDTO> list = service.findAll();
+        return ResponseEntity.ok().body(list);
+    }
+}
