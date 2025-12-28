e8810c44f01b9b4ff3e083bae99079a685bb9238
Marcos Ferreira Shirafuchi <marcosfshirafuchi@gmail.com>
2025-12-28 18:54:33 -0300

    20. Dados de auditoria

---
+++ 01 - CRUD e estruturação do projeto/20. Dados de auditoria/backend/src/main/resources/import.sql
@@ -0,0 +1,3 @@
+INSERT INTO tb_category(name, created_At) VALUES('Books', NOW());
+INSERT INTO tb_category(name, created_At) VALUES('Eletronics', NOW());
+INSERT INTO tb_category(name, created_At) VALUES('Computers', NOW());
