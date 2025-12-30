9dade19e799a0a62b209432ce9336492fcff6b68
Marcos Ferreira Shirafuchi <marcosfshirafuchi@gmail.com>
2025-12-30 00:28:24 -0300

    23. Mapeamento JPA N-N e script SQL completo

---
+++ 01 - CRUD e estruturação do projeto/23. Mapeamento JPA N-N e script SQL completo/backend/src/main/java/com/devsuprior/dscatalog/entities/Category.java
@@ -0,0 +1,83 @@
+package com.devsuprior.dscatalog.entities;
+
+import jakarta.persistence.*;
+
+import java.io.Serializable;
+import java.time.Instant;
+import java.util.Objects;
+
+@Entity
+//Define o nome da tabela do banco de dados
+@Table(name = "tb_category")
+public class Category implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    //Definindo o id da tabela tb_category
+    @Id
+    //Gera automaticamente o número do Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+    private Instant createdAt;
+
+    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+    private Instant updatedAt;
+
+
+    public Category(){
+
+    }
+
+    public Category(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Instant getCreatedAt() {
+        return createdAt;
+    }
+
+    public Instant getUpdatedAt() {
+        return updatedAt;
+    }
+
+    @PrePersist
+    public void prePersist(){
+        createdAt = Instant.now();
+    }
+
+    @PreUpdate
+    public void preUpdate(){
+        updatedAt = Instant.now();
+    }
+
+    @Override
+    public final boolean equals(Object o) {
+        if (!(o instanceof Category category)) return false;
+
+        return Objects.equals(id, category.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}
