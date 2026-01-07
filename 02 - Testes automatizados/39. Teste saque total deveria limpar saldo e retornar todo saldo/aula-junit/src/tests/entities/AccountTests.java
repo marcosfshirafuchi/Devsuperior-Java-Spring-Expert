d6fd8f54a86fdc964bd0a62feefcba3ddb0823f4
Marcos Ferreira Shirafuchi <marcosfshirafuchi@gmail.com>
2026-01-06 23:08:52 -0300

    39. Teste saque total deveria limpar saldo e retornar todo saldo

---
+++ 02 - Testes automatizados/39. Teste saque total deveria limpar saldo e retornar todo saldo/aula-junit/src/tests/entities/AccountTests.java
@@ -0,0 +1,60 @@
+package tests.entities;
+
+import entities.Account;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import tests.factory.AccountFactory;
+
+public class AccountTests {
+
+    @Test
+    //Nomenclatura de um teste
+    //●	<AÇÃO> should <EFEITO> [when <CENÁRIO>]
+    public void depositShouldIncreaseBalanceWhenPositiveAmount(){
+
+        //Padrão AAA
+        //●	Arrange: instancie os objetos necessários
+        double amount = 200.0;
+        double expectedValue = 196.0;
+        Account acc = AccountFactory.createEmptyAccount();
+
+        //●	Act: execute as ações necessárias
+        acc.deposit(amount);
+
+        //●	Assert: declare o que deveria acontecer (resultado esperado)
+        Assertions.assertEquals(expectedValue, acc.getBalance());
+    }
+
+    @Test
+    //Nomenclatura de um teste
+    //●	<AÇÃO> should <EFEITO> [when <CENÁRIO>]
+    public void depositShouldDoNothingNegativeAmount(){
+        //Padrão AAA
+        //●	Arrange: instancie os objetos necessários
+        double expectedValue = 100.0;
+        Account acc = AccountFactory.createAccount(expectedValue);
+        double amount = -200.0;
+
+        //●	Act: execute as ações necessárias
+        acc.deposit(amount);
+
+        //●	Assert: declare o que deveria acontecer (resultado esperado)
+        Assertions.assertEquals(expectedValue, acc.getBalance());
+    }
+
+    @Test
+    public void fullWithdrawShouldClearBalanceAndReturnFullBalance(){
+        //Padrão AAA
+        //●	Arrange: instancie os objetos necessários
+        double expectedValue = 0.0;
+        double initialBalance = 800.0;
+        Account acc = AccountFactory.createAccount(initialBalance);
+
+        //●	Act: execute as ações necessárias
+        double result = acc.fullWithdraw();
+
+        //●	Assert: declare o que deveria acontecer (resultado esperado)
+        Assertions.assertTrue(expectedValue == acc.getBalance());
+        Assertions.assertTrue(result == initialBalance);
+    }
+}
