257d6629f0a30e38b5f57b0b03f1f7c68fdbdd4d
Marcos Ferreira Shirafuchi <marcosfshirafuchi@gmail.com>
2026-04-30 19:11:25 -0300

    82. Resposta customizada para erro de validação

---
+++ 03 - Validação e segurança/82. Resposta customizada para erro de validação/backend/src/main/java/com/devsuprior/dscatalog/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,10 @@
+package com.devsuprior.dscatalog.exceptions;
+
+public class ResourceNotFoundException extends  RuntimeException{
+    private static final long serialVersionUID = 1L;
+
+    public ResourceNotFoundException(String msg){
+        super(msg);
+    }
+
+}
