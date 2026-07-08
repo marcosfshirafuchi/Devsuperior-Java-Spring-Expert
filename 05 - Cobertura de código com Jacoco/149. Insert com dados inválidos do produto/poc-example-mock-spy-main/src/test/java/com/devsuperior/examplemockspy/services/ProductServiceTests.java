6e4cce5a3ee737cf4a053e7513aba5988334edaa
Marcos Ferreira Shirafuchi <marcosfshirafuchi@gmail.com>
2026-07-07 22:13:10 -0300

    149. Insert com dados inválidos do produto

---
+++ 05 - Cobertura de código com Jacoco/149. Insert com dados inválidos do produto/poc-example-mock-spy-main/src/test/java/com/devsuperior/examplemockspy/services/ProductServiceTests.java
@@ -0,0 +1,83 @@
+package com.devsuperior.examplemockspy.services;
+
+import com.devsuperior.examplemockspy.dto.ProductDTO;
+import com.devsuperior.examplemockspy.entities.Product;
+import com.devsuperior.examplemockspy.repositories.ProductRepository;
+import com.devsuperior.examplemockspy.services.exceptions.InvalidDataException;
+import jakarta.persistence.EntityNotFoundException;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+public class ProductServiceTests {
+
+    @InjectMocks
+    private ProductService service;
+
+    @Mock
+    private ProductRepository repository;
+
+    private Long existingId, nonExistingId;
+
+    private Product product;
+
+    private ProductDTO productDTO;
+
+    @BeforeEach
+    void setUp() throws Exception{
+
+        existingId = 1L;
+        nonExistingId = 2L;
+
+
+        product = new Product(1L,"Playstation",2000.0);
+        productDTO = new ProductDTO(product);
+
+        //Cenários para testes da classe ProductService
+        Mockito.when(repository.save(Mockito.any())).thenReturn(product);
+
+        Mockito.when(repository.getReferenceById(existingId)).thenReturn(product);
+
+        Mockito.when(repository.getReferenceById(nonExistingId)).thenThrow(new EntityNotFoundException());
+    }
+
+    @Test
+    public void insertShouldReturnProductDTOWhenValidDate(){
+        //Mockito.spy() vai espionar o service
+        ProductService serviceSpy = Mockito.spy(service);
+        Mockito.doNothing().when(serviceSpy).validateData(productDTO);
+
+        ProductDTO result = serviceSpy.insert(productDTO);
+
+        Assertions.assertNotNull(result);
+        Assertions.assertEquals(result.getName(),"Playstation");
+    }
+
+    @Test
+    public void insertShouldReturnInvalidDataExceptionWhenProductNameIsBlank(){
+        productDTO.setName("");
+        ProductService serviceSpy = Mockito.spy(service);
+        Mockito.doThrow(InvalidDataException.class).when(serviceSpy).validateData(productDTO);
+
+        Assertions.assertThrows(InvalidDataException.class,()->{
+            ProductDTO result = serviceSpy.insert(productDTO);
+        });
+    }
+
+    @Test
+    public void insertShouldReturnInvalidDataExceptionWhenProductPriceIsNegativeOrZero(){
+        productDTO.setPrice(-5.0);
+        ProductService serviceSpy = Mockito.spy(service);
+        Mockito.doThrow(InvalidDataException.class).when(serviceSpy).validateData(productDTO);
+
+        Assertions.assertThrows(InvalidDataException.class,()->{
+            ProductDTO result = serviceSpy.insert(productDTO);
+        });
+    }
+}
