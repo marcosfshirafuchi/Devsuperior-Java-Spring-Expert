7f0f0641a9c45300e7758363ae534d790caee083
Marcos Ferreira Shirafuchi <marcosfshirafuchi@gmail.com>
2026-07-24 17:59:49 -0300

    179. Teste inserindo produtos com dados invalidos

---
+++ 06 - Testes de API com MockMvc e RestAssured/179. Teste inserindo produtos com dados invalidos/dscommerce-jacoco/dscommerce-backend-aulas-cap05-3.4.4/src/test/java/com/devsuperior/dscommerce/controllers/it/ProductControllerIT.java
@@ -0,0 +1,259 @@
+package com.devsuperior.dscommerce.controllers.it;
+
+import com.devsuperior.dscommerce.dto.ProductDTO;
+import com.devsuperior.dscommerce.entities.Category;
+import com.devsuperior.dscommerce.entities.Product;
+import com.devsuperior.dscommerce.tests.TokenUtil;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+//As duas anotações carregam o contexto da aplicação
+@SpringBootTest
+@AutoConfigureMockMvc
+//O Trasanctional da o Rollback no banco de dados
+@Transactional
+public class ProductControllerIT {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private TokenUtil tokenUtil;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    private String clientUsername, clientPassword, adminUsername, adminPassword;
+
+    private String clientToken, adminToken, invalidToken;
+
+    private String productName;
+
+    private Product product;
+
+    private ProductDTO productDTO;
+
+
+    @BeforeEach
+    void setUp() throws Exception {
+        clientUsername = "maria@gmail.com";
+        clientPassword = "123456";
+        adminUsername = "alex@gmail.com";
+        adminPassword = "123456";
+        productName = "Macbook Pro";
+
+        clientToken = tokenUtil.obtainAccessToken(mockMvc, clientUsername, clientPassword);
+        adminToken = tokenUtil.obtainAccessToken(mockMvc, adminUsername, adminPassword);
+        invalidToken = adminToken + "xpto"; // Simulates wrong password
+
+        //Instanciar o método request do Post
+        Category category = new Category(2L, "Eletro");
+        //Inserindo o conteudo da request do método Post
+        product = new Product(null, "Console Playstation 5", "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Qui ad, adipisci illum ipsam velit et odit eaque reprehenderit ex maxime delectus dolore labore, quisquam quae tempora natus esse aliquam veniam doloremque quam minima culpa alias maiores commodi. Perferendis enim", 3999.90, "https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/1-big.jpg");
+        product.getCategories().add(category);
+        productDTO = new ProductDTO(product);
+    }
+    /*Exercícios de fixação: Testes de API com MockMvc*/
+
+    /*Problema 1: Consultar produto por nome
+
+    Implemente o teste de API usando MockMvc para consultar produtos por nome. Desta forma, ao fazer a requisição do tipo GET no endpoint /products?name={productName} onde productName deve ser correspondente a string “Macbook” deve retornar como response o status 200 (Ok) e verificar se os campos id, name, price e imgUrl retornados no jsonPath correspondem aos valores da Figura 1 (abaixo).
+
+    * */
+
+    @Test
+    public void findAllShouldReturnPageWhenNameParamIsNotEmpty() throws Exception {
+        ResultActions result = mockMvc
+                .perform(get("/products?name={productName}", productName)
+                        .accept(MediaType.APPLICATION_JSON));
+
+        result.andExpect(status().isOk());
+        result.andExpect(jsonPath("$.content[0].id").value(3L));
+        result.andExpect(jsonPath("$.content[0].name").value("Macbook Pro"));
+        result.andExpect(jsonPath("$.content[0].price").value(1250.0));
+        result.andExpect(jsonPath("$.content[0].imgUrl").value("https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/3-big.jpg"));
+
+    }
+
+    @Test
+    public void findAllShouldReturnPageWhenNameParamIsEmpty() throws Exception {
+        ResultActions result = mockMvc
+                .perform(get("/products")
+                        .accept(MediaType.APPLICATION_JSON));
+
+        result.andExpect(status().isOk());
+        result.andExpect(jsonPath("$.content[0].id").value(1L));
+        result.andExpect(jsonPath("$.content[0].name").value("The Lord of the Rings"));
+        result.andExpect(jsonPath("$.content[0].price").value(90.5));
+        result.andExpect(jsonPath("$.content[0].imgUrl").value("https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/1-big.jpg"));
+    }
+
+    /*Problema 2: Inserir produto
+
+    Implemente os testes de API usando MockMvc para inserção de produto (método POST do ProductController), considerando os seguintes cenários. Lembre-se de inserir o token no cabeçalho da requisição.
+    1.	Inserção de produto insere produto com dados válidos quando logado como admin
+    2.	Inserção de produto retorna 422 e mensagens customizadas com dados inválidos quando logado como admin e campo name for inválido
+    3.	Inserção de produto retorna 422 e mensagens customizadas com dados inválidos quando logado como admin e campo description for inválido
+    4.	Inserção de produto retorna 422 e mensagens customizadas com dados inválidos quando logado como admin e campo price for negativo
+    5.	Inserção de produto retorna 422 e mensagens customizadas com dados inválidos quando logado como admin e campo price for zero
+    6.	Inserção de produto retorna 422 e mensagens customizadas com dados inválidos quando logado como admin e não tiver categoria associada
+    7.	Inserção de produto retorna 403 quando logado como cliente
+    8.	Inserção de produto retorna 401 quando não logado como admin ou cliente
+
+    * */
+
+    //1.	Inserção de produto insere produto com dados válidos quando logado como admin
+    @Test
+    public void insertShouldReturnProductDTOCreatedWhenAdminLogged() throws Exception {
+        String jsonBody = objectMapper.writeValueAsString(productDTO);
+        ResultActions result = mockMvc
+                //No perform vai testar o metódo get do controller e sua url
+                .perform(post("/products")
+                        //Vai passar o Bearer Token
+                        .header("Authorization", "Bearer " + adminToken)
+                        //Vai passar o corpo da requisição
+                        .content(jsonBody)
+                        //Fala que o formato da requisição é Json
+                        .contentType(MediaType.APPLICATION_JSON)
+                        //O Tipo da response do método
+                        .accept(MediaType.APPLICATION_JSON));
+
+        //Verificar o status
+        result.andExpect(status().isCreated());
+        //Verifica o id do novo produto cadastrado
+        result.andExpect(jsonPath("$.id").exists());
+        //Verifica o name
+        result.andExpect(jsonPath("$.name").value("Console Playstation 5"));
+        //Verifica o description
+        result.andExpect(jsonPath("$.description").value("Lorem ipsum, dolor sit amet consectetur adipisicing elit. Qui ad, adipisci illum ipsam velit et odit eaque reprehenderit ex maxime delectus dolore labore, quisquam quae tempora natus esse aliquam veniam doloremque quam minima culpa alias maiores commodi. Perferendis enim"));
+        //Verifica o price
+        result.andExpect(jsonPath("$.price").value(3999.90));
+        //Verifica o imgUrl
+        result.andExpect(jsonPath("$.imgUrl").value("https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/1-big.jpg"));
+        //Verifica o categories
+        result.andExpect(jsonPath("$.categories[0].id").value(2L));
+
+    }
+
+    //2.	Inserção de produto retorna 422 e mensagens customizadas com dados inválidos quando logado como admin e campo name for inválido
+    @Test
+    public void insertShouldReturnUnprocessableEntityCreatedWhenAdminLoggedAndInvalidName() throws Exception {
+        product.setName("ab");
+        productDTO = new ProductDTO(product);
+        String jsonBody = objectMapper.writeValueAsString(productDTO);
+        ResultActions result =mockMvc
+                //No perform vai testar o metódo get do controller e sua url
+                .perform(post("/products")
+                        //Vai passar o Bearer Token
+                        .header("Authorization", "Bearer " + adminToken)
+                        //Vai passar o corpo da requisição
+                        .content(jsonBody)
+                        //Fala que o formato da requisição é Json
+                        .contentType(MediaType.APPLICATION_JSON)
+                        //O Tipo da response do método
+                        .accept(MediaType.APPLICATION_JSON));
+
+        //Verificar o status
+        result.andExpect(status().isUnprocessableEntity());
+    }
+
+    //3.	Inserção de produto retorna 422 e mensagens customizadas com dados inválidos quando logado como admin e campo description for inválido
+    @Test
+    public void insertShouldReturnUnprocessableEntityCreatedWhenAdminLoggedAndInvalidDescription() throws Exception {
+        product.setDescription("ab");
+        productDTO = new ProductDTO(product);
+        String jsonBody = objectMapper.writeValueAsString(productDTO);
+        ResultActions result =mockMvc
+                //No perform vai testar o metódo get do controller e sua url
+                .perform(post("/products")
+                        //Vai passar o Bearer Token
+                        .header("Authorization", "Bearer " + adminToken)
+                        //Vai passar o corpo da requisição
+                        .content(jsonBody)
+                        //Fala que o formato da requisição é Json
+                        .contentType(MediaType.APPLICATION_JSON)
+                        //O Tipo da response do método
+                        .accept(MediaType.APPLICATION_JSON));
+
+        //Verificar o status
+        result.andExpect(status().isUnprocessableEntity());
+    }
+
+    //4.	Inserção de produto retorna 422 e mensagens customizadas com dados inválidos quando logado como admin e campo price for negativo
+    @Test
+    public void insertShouldReturnUnprocessableEntityCreatedWhenAdminLoggedAndPriceIsNegative() throws Exception {
+        product.setPrice(-50.0);
+        productDTO = new ProductDTO(product);
+        String jsonBody = objectMapper.writeValueAsString(productDTO);
+        ResultActions result =mockMvc
+                //No perform vai testar o metódo get do controller e sua url
+                .perform(post("/products")
+                        //Vai passar o Bearer Token
+                        .header("Authorization", "Bearer " + adminToken)
+                        //Vai passar o corpo da requisição
+                        .content(jsonBody)
+                        //Fala que o formato da requisição é Json
+                        .contentType(MediaType.APPLICATION_JSON)
+                        //O Tipo da response do método
+                        .accept(MediaType.APPLICATION_JSON));
+
+        //Verificar o status
+        result.andExpect(status().isUnprocessableEntity());
+    }
+
+    //5.	Inserção de produto retorna 422 e mensagens customizadas com dados inválidos quando logado como admin e campo price for zero
+    @Test
+    public void insertShouldReturnUnprocessableEntityCreatedWhenAdminLoggedAndPriceIsZero() throws Exception {
+        product.setPrice(0.0);
+        productDTO = new ProductDTO(product);
+        String jsonBody = objectMapper.writeValueAsString(productDTO);
+        ResultActions result =mockMvc
+                //No perform vai testar o metódo get do controller e sua url
+                .perform(post("/products")
+                        //Vai passar o Bearer Token
+                        .header("Authorization", "Bearer " + adminToken)
+                        //Vai passar o corpo da requisição
+                        .content(jsonBody)
+                        //Fala que o formato da requisição é Json
+                        .contentType(MediaType.APPLICATION_JSON)
+                        //O Tipo da response do método
+                        .accept(MediaType.APPLICATION_JSON));
+
+        //Verificar o status
+        result.andExpect(status().isUnprocessableEntity());
+    }
+    //6.	Inserção de produto retorna 422 e mensagens customizadas com dados inválidos quando logado como admin e não tiver categoria associada
+    @Test
+    public void insertShouldReturnUnprocessableEntityCreatedWhenAdminLoggedAndProductHasNotCategory() throws Exception {
+        product.getCategories().clear();
+        productDTO = new ProductDTO(product);
+        String jsonBody = objectMapper.writeValueAsString(productDTO);
+        ResultActions result =mockMvc
+                //No perform vai testar o metódo get do controller e sua url
+                .perform(post("/products")
+                        //Vai passar o Bearer Token
+                        .header("Authorization", "Bearer " + adminToken)
+                        //Vai passar o corpo da requisição
+                        .content(jsonBody)
+                        //Fala que o formato da requisição é Json
+                        .contentType(MediaType.APPLICATION_JSON)
+                        //O Tipo da response do método
+                        .accept(MediaType.APPLICATION_JSON));
+
+        //Verificar o status
+        result.andExpect(status().isUnprocessableEntity());
+    }
+}
